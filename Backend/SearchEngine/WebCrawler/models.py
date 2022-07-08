9dbaeb8a4743896cd6a640c406ba752efff97e93
Prital Bamnodkar <pritalbamnodkar@gmail.com>
2022-07-08 21:28:15 +0530

    added __str__ method to the models

--- Backend/SearchEngine/WebCrawler/models.py
+++ Backend/SearchEngine/WebCrawler/models.py
@@ -7,12 +7,24 @@ class Url(models.Model):
     title = models.TextField()
     backlinks = models.IntegerField()
 
+    def __str__(self):
+        return self.title
+
 class Index(models.Model):
     keyword = models.TextField(verbose_name='keyword')
     urls = models.ManyToManyField('Url', related_name='keywords' ,symmetrical=False)
 
+    def __str__(self):
+        return self.keyword
+
 class CrawlFrontier(models.Model):
     url =models.URLField()
 
+    def __str__(self):
+        return self.url
+
 class CrawledSites(models.Model):
     url = models.URLField()
+    
+    def __str__(self):
+        return self.url
