261cd6ea8c2dce9e79515e7c53ba8c3a2da2f216
Prital Bamnodkar <pritalbamnodkar@gmail.com>
2022-07-09 14:02:39 +0530

    search api implemented

--- Backend/SearchEngine/Api/views.py
+++ Backend/SearchEngine/Api/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 # Create your views here.
+from Search import utils
+
+@api_view(['GET',])
+def search(request, query):
+    keywords = query.split(' ')
+    results = utils.search(keywords)
+
+    return Response(results)
